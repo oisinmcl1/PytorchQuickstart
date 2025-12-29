171f766c1d6c3db43c5368d77553c8eb42e986b7
oisinmcl1 <omclaughlin99@gmail.com>
2025-12-29 14:54:10 +0000

    running model

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ print(model)
 
 # ===== Training and  Test =====
 
-def train(dataloader, model, loss_fn, optimizer):
+def train(dataloader, model, loss_fn, optimiser):
     size = len(dataloader.dataset)
     model.train()
 
@@ -83,8 +83,8 @@ def train(dataloader, model, loss_fn, optimizer):
 
         # Backpropagation
         loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+        optimiser.step()
+        optimiser.zero_grad()
 
         # Print loss every 100 batches
         if batch % 100 == 0:
@@ -112,3 +112,14 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 # ==========
+
+# ===== Run Model =====
+loss_fn = nn.CrossEntropyLoss()
+optimiser = torch.optim.SGD(model.parameters(), lr=1e-3)
+epochs = 5
+
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimiser)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
